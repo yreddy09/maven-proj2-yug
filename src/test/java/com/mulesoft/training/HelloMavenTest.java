60f3dffebdb0d2a9c671e1f713935a49757a864c
Yugandhar Reddy Thippireddy <yreddy@Yugandhars-MacBook-Pro.local>
2018-08-20 17:27:46 -0500

    Failing the test case

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -9,7 +9,7 @@ public class HelloMavenTest extends FunctionalTestCase {
 
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
-        runFlowAndExpect("mavenFlow", "Hello World by Yug");
+        runFlowAndExpect("mavenFlow", "Hello World by Yug2");
     }
     
     @Override
